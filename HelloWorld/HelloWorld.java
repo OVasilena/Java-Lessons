b58c57321f4e58a3407c734a77a4d53679c6d850
OVasilena <olgavasilena@gmail.com>
2017-01-21 10:39:33 +0100

    HelloWorld
    создание репозитория

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,17 @@
+package HelloWorld;
+
+/**
+ * Created by olechka on 20.01.2017.
+ */
+public class HelloWorld {
+    public static void main (String[] args){
+        int x = 5;
+        int y = 500_000;
+        int z = 450_000;
+        System.out.println("Hello World");
+        System.out.println("\u0423\u0440\u0430 \n");
+        System.out.println("\n\t"+"Ура"+" "+"+"+"\t ="+ x);
+        System.out.println("УРА"+" "+500.000);
+        System.out.println(y-z);
+    }
+}
